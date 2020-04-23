4b02b27e98c29c8d6198751be7007c81183f22bc
gapsong <khiemtonthat@yahoo.de>
2020-04-23 13:48:56 +0200

    wrote unit tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,20 @@
-
+import { convertDurationtoSeconds } from '../lib/commentFetch'
+var expect = require('expect.js')
 
 describe('Example Node Server', () => {
-  it('should return 200', done => {
-    done();
-  });
-});
+    it('should return 200', (done) => {
+        done()
+    })
+})
+
+describe('converts Time correctly', () => {
+    it('should return correct seconds', () => {
+        expect(convertDurationtoSeconds('2h44m2s')).to.equal(9842)
+    })
+    it('should test without seconds', () => {
+        expect(convertDurationtoSeconds('2h44m')).to.equal(9840)
+    })    
+    it('should test without hours and without seconds', () => {
+        expect(convertDurationtoSeconds('44m')).to.equal(2640)
+    })
+})
