62b94d2e2721083de5c992236049f1684cb1e66e
gapsong <khiemtonthat@yahoo.de>
2020-04-30 20:24:37 +0200

    udpated tests

--- test/statsEngine.js
+++ test/statsEngine.js
@@ -1,5 +1,6 @@
 import expect from 'expect.js'
 import {
+    getActivityPeaks,
     getSlidingAverage,
     getAverageCountFromTo,
     getEntriesPerSeconds,
@@ -24,6 +25,29 @@ describe('getEntriesPerSeconds()', () => {
                 content_offset_seconds: 4,
             },
         ]
+
+        expect(getEntriesPerSeconds(mockData)).to.eql([2, 0, 2, 0, 1])
+    })
+    
+    it('should return an array with comments per second', () => {
+        const mockData = [
+            {
+                content_offset_seconds: 0.5,
+            },
+            {
+                content_offset_seconds: 0.5,
+            },
+            {
+                content_offset_seconds: 2,
+            },
+            {
+                content_offset_seconds: 2.5,
+            },
+            {
+                content_offset_seconds: 4.3,
+            },
+        ]
+
         expect(getEntriesPerSeconds(mockData)).to.eql([2, 0, 2, 0, 1])
     })
 
@@ -54,10 +78,12 @@ describe('getEntriesPerSeconds()', () => {
 describe('getAverageCountFromTo()', () => {
     it('test 1', () => {
         const mockData = [1, 3, 5, 3, 2, 8, 6, 5, 3, 2]
+
         expect(getAverageCountFromTo(mockData, 0, 5)).to.equal(2.8)
     })
     it('test 2', () => {
         const mockData = [1, 3, 5, 3, 2, 8, 6, 5, 3, 2]
+
         expect(getAverageCountFromTo(mockData, 0, 6)).to.equal(3.66)
     })
 })
@@ -68,12 +94,14 @@ describe('getSlidingAverage()', () => {
     it('test 0', () => {
         const mockData = [1]
         const windowSize = 4
+
         expect(getSlidingAverage(mockData, windowSize)).to.eql([])
     })
 
     it('test 1', () => {
         const mockData = [1, 3, 5, 3, 2, 8, 6, 5, 3, 2]
         const windowSize = 4
+
         expect(getSlidingAverage(mockData, windowSize)).to.eql([
             3,
             3.25,
@@ -88,6 +116,7 @@ describe('getSlidingAverage()', () => {
     it('test 2', () => {
         const mockData = [1, 3, 0, 3, 0, 8, 6, 5, 3, 12]
         const windowSize = 2
+
         expect(getSlidingAverage(mockData, windowSize)).to.eql([
             2,
             1.5,
@@ -104,9 +133,10 @@ describe('getSlidingAverage()', () => {
 
 describe('getActivityPeaks()', () => {
     it('test 1', () => {
-        const multiplier = 4
-        const mockData = []
+        const threshold = 4
+        const mockData = [1, 3, 5, 3, 2, 8, 6, 5, 3, 2]
+        const sldingAverage = [3, 3.25, 4.5, 4.75, 5.25, 5.5, 4]
 
-        expect(getActivityPeaks(mockData)).to.eql([])
+        expect(getActivityPeaks(mockData, sldingAverage, threshold)).to.eql([])
     })
 })
