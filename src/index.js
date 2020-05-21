5724c5693555d7479f397ac5ad02ee9109f97ed7
gapsong <khiemtonthat@yahoo.de>
2020-05-21 21:32:57 +0200

    refactored things

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+import fs from 'fs'
+
 import { fetchTwitchChatById } from './lib/fetchTwitchChat'
+import { sumArray, getEntriesPerSeconds } from './lib/statsUtils'
 
 const VIDEO_ID = 627447662
 
-fetchTwitchChatById(VIDEO_ID)
+// fetchTwitchChatById(VIDEO_ID)
 
-// var jsonContent = JSON.stringify(sumArray(getEntriesPerSeconds(comments), 10))
-// console.log(jsonContent)
+const chatHistory = require(`../${VIDEO_ID}.json`)
+const level1 = getEntriesPerSeconds(chatHistory.comments)
+const level2 = sumArray(level1, 10)
+const jsonContent = JSON.stringify({ videoId: VIDEO_ID, comments: level2 })
 
-// fs.writeFile('output.json', jsonContent, 'utf8', function (err) {
-//     if (err) {
-//         console.log('An error occured while writing JSON Object to File.')
-//         return console.log(err)
-//     }
+fs.writeFile('output.json', jsonContent, 'utf8', function (err) {
+    if (err) {
+        console.log('An error occured while writing JSON Object to File.')
+        return console.log(err)
+    }
 
-//     console.log('JSON file has been saved.')
-// })
+    console.log('JSON file has been saved.')
+})
