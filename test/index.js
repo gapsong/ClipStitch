a37f3ce4b5ad045ebdd7a21d0dc49896d6fbfe61
gapsong <khiemtonthat@yahoo.de>
2020-05-21 17:12:14 +0200

    renamedfiles

--- test/index.js
+++ test/index.js
@@ -1,22 +1,10 @@
 import expect from 'expect.js'
 import {
     convertDurationtoSeconds,
-} from '../lib/commentFetch'
+} from '../lib/fetchTwitchChat'
 
 describe('Example Node Server', () => {
     it('should return 200', (done) => {
         done()
-    })
-})
-
-describe('converts Time correctly', () => {
-    it.skip('should return correct seconds', () => {
-        expect(convertDurationtoSeconds('2h44m2s')).to.equal(9842)
-    })
-    it.skip('should test without seconds', () => {
-        expect(convertDurationtoSeconds('2h44m0s')).to.equal(9840)
-    })
-    it.skip('should test without hours and without seconds', () => {
-        expect(convertDurationtoSeconds('44m0s')).to.equal(2640)
     })
 })
