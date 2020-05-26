5c260da4309954f60d00b7412245ed95a1f8b564
gapsong <khiemtonthat@yahoo.de>
2020-05-26 17:27:07 +0200

    removed subfolder2 g

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
-import fs from 'fs'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import { fetchTwitchChatById } from './lib/fetchTwitchChat'
-import { sumArray, getEntriesPerSeconds } from './lib/statsUtils'
-import { filterCommentsByWords } from './lib/utils'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const VIDEO_ID = 621148192
-
-const callback = () => {
-    const rawComments = require(`./../ClipStitchVisualizer/src/chatCollection/rawData/${VIDEO_ID}.json`)
-    const filteredTags = ['LOL', 'LULW', 'KEKW', 'WTF', 'LMAO', 'lol', 'clip', 'OMEGALUL']
-    const level0 = filterCommentsByWords(rawComments.comments, filteredTags)
-    const level1 = getEntriesPerSeconds(level0)
-    const level2 = sumArray(level1, 10)
-    const jsonContent = JSON.stringify(
-        {
-            videoId: VIDEO_ID,
-            filteredTags: filteredTags,
-            rawComments: rawComments.comments,
-            entriesPerSeconds: level1,
-            filteredComments: level2,
-        },
-        null,
-        4
-    )
-
-    fs.writeFile(`./ClipStitchVisualizer/src/chatCollection/modified/${VIDEO_ID}.json`, jsonContent, 'utf8', function (err) {
-        if (err) {
-            console.log('An error occured while writing JSON Object to File.')
-            return console.log(err)
-        }
-
-        console.log('Alle Filter wurden eingebaut')
-    })
-}
-
-fetchTwitchChatById(VIDEO_ID, callback)
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
