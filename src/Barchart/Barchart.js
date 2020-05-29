2d2a4403d70736fd6d48263e48ba3e6fc7e5d556
gapsong <khiemtonthat@yahoo.de>
2020-05-30 01:18:26 +0200

    fixed some things2 2 g

--- src/Barchart/Barchart.js
+++ src/Barchart/Barchart.js
@@ -2,7 +2,12 @@ import React from 'react'
 import * as ui from './ui'
 
 const Barchart = ({ data }) => {
-    const max = Math.max(...data.entriesPerSeconds)
+    const max = data.comments.reduce((acc, item) => {
+        if (item.raw > acc) {
+            return item.raw
+        }
+        return acc
+    }, 0)
 
     return (
         <ui.Wrapper>
@@ -13,17 +18,16 @@ const Barchart = ({ data }) => {
                 return item + ', '
             })}
             <ui.Scrollview>
-                {data.entriesPerSeconds.map((item, index) => {
+                {data.comments.map((item, index) => {
                     return (
                         <ui.Col
                             onClick={() => {
                                 window.open(`https://www.twitch.tv/videos/${data.videoId}?t=${(index - 1) * 10}s`, '_blank')
                             }}
                         >
-                            <ui.Bar height={(item - data.filteredComments[index]) / max} />
-                            <ui.Bar height={data.filteredComments[index] / max} /> {item}
+                            {<ui.Bar height={item.raw / max} />}
                             <br />
-                            {data.filteredComments[index]}
+                            {item.raw}
                         </ui.Col>
                     )
                 })}
