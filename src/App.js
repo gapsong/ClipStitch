72213386ee54681942128a424cd3cb5c644847e0
gapsong <khiemtonthat@yahoo.de>
2020-06-04 20:45:28 +0200

    fixed paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Barchart from './Barchart'
-import data from './chatCollection/modified/640810556.json'
+import data from './Barchart/chatCollection/modified/621148192.json'
 
 function App() {
     return (
